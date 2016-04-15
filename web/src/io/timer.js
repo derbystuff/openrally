bbff28758b1096238a33c808ccb3be3caffd51ac
Jeremy Darling <jeremy.darling@gmail.com>
2016-04-15 17:15:37 -0500

    Remove rogue console.log statements

--- web/src/io/timer.js
+++ web/src/io/timer.js
@@ -2,15 +2,11 @@ const store = require('../reducers');
 const io = require('../lib/io');
 
 io.on('timer-status', (status)=>{
-  console.log('Status: ', status);
   store.dispatch({type: 'UPDATE_TIMER_STATUS', status});
-  console.log(store.getState())
 });
 io.on('timer-winner', (winner)=>{
-  console.log('Winner: ', winner);
   store.dispatch({type: 'UPDATE_TIMER_WINNER', winner});
 });
 io.on('timer-error', (error)=>{
-  console.log('Error: ', error);
   store.dispatch({type: 'TIMER_ERROR', error});
 });
