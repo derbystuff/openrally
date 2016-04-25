4f48454b898194e37288f1cc3fc0879771613f1c
Jeremy Darling <jeremy.darling@gmail.com>
2016-04-25 16:30:46 -0500

    Bug fixes to timer and config loading

--- configs/webpack.client.js
+++ configs/webpack.client.js
@@ -3,5 +3,6 @@ var ENV_CONFIG_LOOKUP = {
   production: 'prod',
   prd: 'prod'
 };
+
 var ENV = ENV_CONFIG_LOOKUP[process.env.NODE_ENV||'dev']||'dev';
 module.exports = require('./webpack.'+ENV+'.js');
